dd199b7051918ac7a785c26ea57fe237153f7a48
Douglas Shinzato <shinzato.douglas@gmail.com>
2025-02-01 20:14:01 -0400

    added sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,1 +1,15 @@
 //layout com a sidebar
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { AppSidebar } from '@/components/app-sidebar'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        {/* <SidebarTrigger /> */}
+        {children}
+      </main>
+    </SidebarProvider>
+  )
+}
